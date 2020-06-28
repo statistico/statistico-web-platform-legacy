b4a173c73459b9e9e45ee196c50ad52003270169
Joe Sweeny <joesweeny@intelligencefusion.co.uk>
2020-06-28 17:39:33 +0100

    Implement custom menu toggle hook

--- src/container/Layout/Layout.js
+++ src/container/Layout/Layout.js
@@ -1,22 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 
 import Aux from '../../hoc/Aux/Aux';
 import SidebarMenu from "../../components/Navigation/SidebarMenu/SidebarMenu";
 import TeamStats from '../TeamStats/TeamStats';
 import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
+import { useTogglesMenu } from "../../hooks/useTogglesMenu";
 import classes from './Layout.module.css';
 
 const Layout = () => {
-    const [sidebarMenuOpen, setSidebarMenuOpen] = useState(false);
-
-    const sidebarToggleHandler = () => {
-        setSidebarMenuOpen(!sidebarMenuOpen);
-    }
+    const {menuOpen, menuToggleHandler} = useTogglesMenu(false);
 
     return (
         <Aux>
-            <Toolbar open={sidebarMenuOpen} clicked={sidebarToggleHandler}/>
-            <SidebarMenu open={sidebarMenuOpen} />
+            <Toolbar open={menuOpen} clicked={menuToggleHandler}/>
+            <SidebarMenu open={menuOpen} />
             <main className={classes.Layout}>
                 <TeamStats />
             </main>
--- src/container/TeamStats/TeamStats.js
+++ src/container/TeamStats/TeamStats.js
@@ -1,22 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 
 import Dashboard from '../../components/Dashboard/Dashboard';
 import SidebarMax from '../../components/Sidebar/SidebarMax/SidebarMax';
 import SidebarMin from '../../components/Sidebar/SidebarMin/SidebarMin';
+import { useTogglesMenu } from "../../hooks/useTogglesMenu";
 import classes from './TeamStats.module.css';
 
 const TeamStats = () => {
-    const [sidebarOpen, setSidebarOpen] = useState(true);
-
-    const sidebarToggleHandler = () => {
-        setSidebarOpen(!sidebarOpen);
-    }
+    const {menuOpen, menuToggleHandler} = useTogglesMenu(true);
 
     return (
         <div className={classes.TeamStats}>
-            { sidebarOpen ?
-                <SidebarMax clicked={sidebarToggleHandler} open={sidebarOpen} /> :
-                <SidebarMin clicked={sidebarToggleHandler} open={sidebarOpen} /> }
+            { menuOpen ?
+                <SidebarMax clicked={menuToggleHandler} open={menuOpen} /> :
+                <SidebarMin clicked={menuToggleHandler} open={menuOpen} /> }
             <Dashboard />
         </div>
     )
