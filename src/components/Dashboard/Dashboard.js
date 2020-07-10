99a16cb9d7e92c58035ed63c59a238e3f2195c23
Joe Sweeny <joesweeny@intelligencefusion.co.uk>
2020-07-10 16:29:30 +0100

    Reinstate dashboard component

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,21 +1,11 @@
 import React from 'react';
 
 import classes from './Dashboard.module.css';
-import useFetchesTeamResult from '../../hooks/useFetchesTeamResults';
 
-const Dashboard = () => {
-  const payload = {
-    team: {
-      id: 1,
-      venue: 'home',
-    },
-    limit: 1,
-  };
+const dashboard = () => (
+  <div className={classes.Dashboard}>
+    <p>Dashboard</p>
+  </div>
+);
 
-  const { results, loading } = useFetchesTeamResult(payload);
-
-  const res = results.map((result) => <p>{result.homeTeam.name}</p>);
-  return <div className={classes.Dashboard}>{res}</div>;
-};
-
-export default Dashboard;
+export default dashboard;
