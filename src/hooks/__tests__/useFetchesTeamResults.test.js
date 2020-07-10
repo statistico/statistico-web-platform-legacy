2280349415cdce1101a67913024a2d1a5ad5277c
Joe Sweeny <joesweeny@intelligencefusion.co.uk>
2020-07-10 16:40:17 +0100

    Remove unused dependencies

--- src/hooks/__tests__/useFetchesTeamResults.test.js
+++ src/hooks/__tests__/useFetchesTeamResults.test.js
@@ -2,7 +2,7 @@ import { renderHook } from '@testing-library/react-hooks';
 
 import useAsyncError from '../useAsyncError';
 import useFetchesTeamResult from '../useFetchesTeamResults';
-import { getTeamResults } from '../../packages/api/statistico-client';
+import { getTeamResults } from '../../packages/api';
 
 jest.mock('../../packages/api/statistico-client');
 jest.mock('../useAsyncError');
@@ -23,6 +23,34 @@ describe('useFetchesTeamResult', () => {
     await waitForNextUpdate();
 
     expect(result.current.results).toEqual(results);
-    // expect(result.current.loading).toBe(false);
+    expect(result.current.loading).toBe(false);
+  });
+
+  it('returns loading true on first render', async () => {
+    getTeamResults.mockImplementationOnce(() => Promise.resolve([]));
+
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useFetchesTeamResult({})
+    );
+
+    expect(result.current.loading).toBe(true);
+
+    await waitForNextUpdate();
+
+    expect(result.current.results).toEqual([]);
+    expect(result.current.loading).toBe(false);
+  });
+
+  it('throws an error if thrown by getTeamResults call', async () => {
+    const error = new Error('Not found');
+    getTeamResults.mockImplementationOnce(() => Promise.reject(error));
+
+    useAsyncError.mockImplementation(() => {
+      throw error;
+    });
+
+    const { result } = renderHook(() => useFetchesTeamResult({}));
+
+    expect(result.error).toEqual(error);
   });
 });
