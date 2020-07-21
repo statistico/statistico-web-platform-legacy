ecd4099729c5c0ead492d212a789fc4d596290cc
Joe Sweeny <joesweeny@intelligencefusion.co.uk>
2020-07-21 13:26:17 +0100

    Remove props from team stats sidebar

--- src/components/TeamStats/TeamStats.js
+++ src/components/TeamStats/TeamStats.js
@@ -8,22 +8,16 @@ import TeamStatsSideBar from './TeamStatsSideBar/TeamStatsSideBar';
 import Wrapper from '../Wrapper/Wrapper';
 import useFetchesTeam from '../../hooks/useFetchesTeam';
 import classes from './TeamStats.module.css';
-import useTogglesActiveState from '../../hooks/useTogglesActiveState';
 
 const TeamStats = () => {
   const teamId = 1;
   const seasonId = 16036;
   const { team, loading } = useFetchesTeam(teamId);
-  const { selected, selectionToggleHandler } = useTogglesActiveState('Results');
 
   return (
     <div className={classes.TeamStats}>
       <Loader loading={loading}>
-        <TeamStatsSideBar
-          team={team}
-          selected={selected}
-          setSelected={selectionToggleHandler}
-        />
+        <TeamStatsSideBar team={team} />
         <Dashboard>
           <Wrapper styles={classes.ResultList}>
             <ErrorBoundary>
