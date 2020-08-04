40dcc047c4a2c9f73cd988841657d9b301869728
Joe Sweeny <joesweeny@intelligencefusion.co.uk>
2020-08-04 16:19:43 +0100

    Remove routes from app component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
 
 import Layout from './container/Layout/Layout';
-import Routes from './components/Routes/Routes';
 
 const App = () => (
   <div>
-    <Layout>
-      <Routes />
-    </Layout>
+    <Layout />
   </div>
 );
 
