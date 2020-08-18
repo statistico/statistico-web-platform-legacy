6c4c538bc4dfbbb9d50155078e5735f7a6d96609
Joe Sweeny <joesweeny@intelligencefusion.co.uk>
2020-08-18 20:34:26 +0100

    Fix styling issue

--- src/hooks/useFetchesTeamSeasons.js
+++ src/hooks/useFetchesTeamSeasons.js
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react';
 
 import { teamSeasonsPresenter } from '../presenters/team';
 import useAsyncError from './useAsyncError';
