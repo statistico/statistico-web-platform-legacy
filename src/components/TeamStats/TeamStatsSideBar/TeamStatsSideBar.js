ec66b71bc3c8681ed85e36ea87053c3659a2efd0
Joe Sweeny <joesweeny@intelligencefusion.co.uk>
2020-08-20 20:32:08 +0100

    Move constants out of component

--- src/components/TeamStats/TeamStatsSideBar/TeamStatsSideBar.js
+++ src/components/TeamStats/TeamStatsSideBar/TeamStatsSideBar.js
@@ -9,6 +9,47 @@ import SidebarTab from '../../Sidebar/SidebarTab/SidebarTab';
 import TeamDetails from '../../TeamDetails/TeamDetails';
 import useTogglesMenu from '../../../hooks/useTogglesMenu';
 
+const selectStyles = {
+  container: (provided) => ({
+    ...provided,
+    width: '80%',
+    textAlign: 'center',
+    margin: '0 0 10px 0',
+    padding: '10px 0 10px 0',
+  }),
+
+  valueContainer: (provided) => ({
+    ...provided,
+    justifyContent: 'center',
+    color: '#22ccde',
+  }),
+
+  singleValue: (provided) => ({
+    ...provided,
+    fontWeight: 'bold',
+    paddingLeft: '30px',
+  }),
+};
+
+const options = [
+  {
+    id: 0,
+    name: 'MATCHES',
+  },
+  {
+    id: 1,
+    name: 'PLAYERS',
+  },
+  {
+    id: 2,
+    name: 'STATS',
+  },
+  {
+    id: 3,
+    name: 'TIMELINES',
+  },
+];
+
 const TeamStatsSideBar = (props) => {
   const {
     selected,
@@ -20,47 +61,6 @@ const TeamStatsSideBar = (props) => {
   } = props;
 
   const { menuOpen, menuToggleHandler } = useTogglesMenu(true);
-
-  const options = [
-    {
-      id: 0,
-      name: 'MATCHES',
-    },
-    {
-      id: 1,
-      name: 'PLAYERS',
-    },
-    {
-      id: 2,
-      name: 'STATS',
-    },
-    {
-      id: 3,
-      name: 'TIMELINES',
-    },
-  ];
-
-  const selectStyles = {
-    container: (provided) => ({
-      ...provided,
-      width: '80%',
-      textAlign: 'center',
-      margin: '0 0 10px 0',
-      padding: '10px 0 10px 0',
-    }),
-
-    valueContainer: (provided) => ({
-      ...provided,
-      justifyContent: 'center',
-      color: '#22ccde',
-    }),
-
-    singleValue: (provided) => ({
-      ...provided,
-      fontWeight: 'bold',
-      paddingLeft: '30px',
-    }),
-  };
 
   return (
     <Aux>
