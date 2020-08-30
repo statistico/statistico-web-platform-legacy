a00f94577304bcf239cc8cf9daf622ffec8ed3e8
Joe Sweeny <joesweeny@intelligencefusion.co.uk>
2020-08-30 15:07:21 +0100

    Refactor styling

--- src/components/Team/TeamStats/TeamStatsCard/TeamStatsCounts/TeamStatsCounts.js
+++ src/components/Team/TeamStats/TeamStatsCard/TeamStatsCounts/TeamStatsCounts.js
@@ -16,8 +16,8 @@ const teamStatsCounts = ({ stats }) => {
   }
 
   const filtered = stats.map((item) => item.value);
-  const total = filtered.reduce((prev, curr) => prev + curr, 0);
-  const average = total / stats.length;
+  const sum = filtered.reduce((prev, curr) => prev + curr, 0);
+  const average = sum / stats.length;
   const max = calculateMax(filtered);
   const median = calculateMedian(filtered);
   const mode = calculateMode(filtered);
@@ -30,15 +30,15 @@ const teamStatsCounts = ({ stats }) => {
           <CountUp start={0} end={stats.length} duration={2} />
         </div>
         <div className={classes.Count}>
-          <h3>Total</h3>
-          <CountUp start={0} end={total} duration={2} />
+          <h3>Sum</h3>
+          <CountUp start={0} end={sum} duration={2} />
         </div>
-      </div>
-      <div className={classes.Calculated}>
         <div className={classes.Count}>
           <h3>Average</h3>
           <CountUp start={0} end={average} decimals={2} duration={2} />
         </div>
+      </div>
+      <div className={classes.Totals}>
         <div className={classes.Count}>
           <h3>Max</h3>
           <CountUp start={0} end={max} duration={2} />
