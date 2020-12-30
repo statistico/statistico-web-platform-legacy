c3975377f2dc0ae9bd507d4cbfaf520d5b9dc005
Joe Sweeny <joesweeny@intelligencefusion.co.uk>
2020-12-30 13:04:55 +0000

    Add price filter to strategy filter panel

--- src/components/Trading/StrategySearch/StrategyFilterPanel/StrategyFilterPanel.js
+++ src/components/Trading/StrategySearch/StrategyFilterPanel/StrategyFilterPanel.js
@@ -1,8 +1,29 @@
 import React, { useCallback, useState } from 'react';
 
+import PriceFilter from './PriceFilter/PriceFilter';
 import SingleFilter from '../../../SingleFilter/SingleFilter';
 import StrategyFilterPanelWrapper from './StrategyFilterPanelWrapper';
-import { markets } from '../../../../config/config';
+import markets from '../../../../config/markets';
+
+const styles = {
+  container: (provided) => ({
+    ...provided,
+    width: '100%',
+    textAlign: 'left',
+    fontSize: '18px',
+  }),
+
+  valueContainer: (provided) => ({
+    ...provided,
+    justifyContent: 'left',
+    color: '#1d1d1d',
+  }),
+
+  control: (provided) => ({
+    ...provided,
+    cursor: 'pointer',
+  }),
+};
 
 const StrategyFilterPanel = () => {
   const [market, toggleMarket] = useState(null);
@@ -18,16 +39,18 @@ const StrategyFilterPanel = () => {
       <SingleFilter
         selection={market}
         selections={markets}
+        styles={styles}
         title="Market"
         toggleSelection={updateMarket}
       />
       <SingleFilter
         selection={runner}
         selections={market != null ? market.runners : []}
+        styles={styles}
         title="Selection"
         toggleSelection={toggleRunner}
       />
-      <div>Min and Max Price Filters here</div>
+      <PriceFilter />
       <div>League Filters here</div>
       <div>Result Filters here</div>
       <div>Stat Filters here</div>
