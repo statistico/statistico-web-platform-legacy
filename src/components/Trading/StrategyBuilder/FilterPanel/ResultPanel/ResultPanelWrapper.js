6c0e5499d9e97caadd558db0f12f8c01a27643a9
Joe Sweeny <joesweeny@intelligencefusion.co.uk>
2021-03-07 15:37:34 +0000

    Add result rules panel

--- src/components/Trading/StrategyBuilder/FilterPanel/ResultPanel/ResultPanelWrapper.js
+++ src/components/Trading/StrategyBuilder/FilterPanel/ResultPanel/ResultPanelWrapper.js
@@ -1,18 +1,50 @@
 import styled from 'styled-components';
 
-const ResultPanelWrapper = styled.div`
+export const ResultPanelWrapper = styled.div`
   display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
   width: 39%;
   border-radius: 10px;
   background-color: #1b1e22;
   box-shadow: 5px 5px 5px 0px rgba(0, 0, 0, 0.23);
-  height: 350px;
+  height: fit-content;
   margin: 20px 0 20px 0;
+  padding: 15px;
+
+  table {
+    width: 100%;
+    text-align: left;
+    margin-left: 20px;
+    font-size: 18px;
+  }
+
+  svg {
+    color: #22ccde;
+    cursor: pointer;
+
+    &:hover {
+      color: #ffffff;
+    }
+  }
 
   @media (max-width: 768px) {
     width: 98%;
     margin: 0 0 20px 0;
+
+    table {
+      text-align: left;
+      margin-left: 10px;
+      font-size: 14px;
+    }
   }
 `;
 
-export default ResultPanelWrapper;
+export const ResultRuleWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  width: 100%;
+`;
---
+++ src/components/Trading/StrategyBuilder/FilterPanel/ResultPanel/ResultRule/ResultRuleWrapper.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const ResultRuleWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+export default ResultRuleWrapper;
