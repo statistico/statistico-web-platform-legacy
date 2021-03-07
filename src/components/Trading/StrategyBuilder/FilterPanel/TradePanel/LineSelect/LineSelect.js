6ed10179cf0b98ae40eb13c6b80f9acfc0147688
Joe Sweeny <joesweeny@intelligencefusion.co.uk>
2021-03-07 09:06:05 +0000

    Add filter functionality to line market and odds filters

--- src/components/Trading/StrategyBuilder/FilterPanel/TradePanel/LineSelect/LineSelect.js
+++ src/components/Trading/StrategyBuilder/FilterPanel/TradePanel/LineSelect/LineSelect.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import LineSelectWrapper from './LineSelectWrapper';
 import SingleFilter from '../../../../../SingleFilter/SingleFilter';
+import {
+  StrategyBuilderActionContext,
+  StrategyBuilderContext,
+} from '../../../../../../context/StrategyBuilderContext';
 
 import { lines } from '../../../../../../config/filters';
 import selectStyles from '../../../../../../config/form-styles';
 
 const LineSelect = () => {
+  const { filters } = useContext(StrategyBuilderContext);
+  const { setFilters } = useContext(StrategyBuilderActionContext);
+
+  const updateLine = (l) => {
+    setFilters({ ...filters, line: l });
+  };
+
   return (
     <LineSelectWrapper>
       <SingleFilter
-        selection={null}
+        selection={filters.line}
         selections={lines}
         styles={selectStyles}
         title="Line"
-        toggleSelection={() => {}}
+        toggleSelection={updateLine}
       />
     </LineSelectWrapper>
   );
