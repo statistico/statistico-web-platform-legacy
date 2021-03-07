866beb3602d5aa320f0377d9c3ae184ed6965083
Joe Sweeny <joesweeny@intelligencefusion.co.uk>
2021-03-07 19:54:54 +0000

    Add filter and trade toggles

---
+++ src/components/Trading/StrategyBuilder/TradePanel/TradePanel.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import TradePanelWrapper from './TradePanelWrapper';
+
+const TradePanel = () => {
+  return (
+    <TradePanelWrapper>
+      <div>This is the trade panel</div>
+    </TradePanelWrapper>
+  );
+};
+
+export default TradePanel;
