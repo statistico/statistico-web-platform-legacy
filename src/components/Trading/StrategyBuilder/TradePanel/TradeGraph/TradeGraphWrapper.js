8a462405649ece563784856f2450cb23192447e4
Joe Sweeny <joesweeny@intelligencefusion.co.uk>
2021-03-08 13:01:03 +0000

    Add trade stat panel

--- src/components/Trading/StrategyBuilder/TradePanel/TradeGraph/TradeGraphWrapper.js
+++ src/components/Trading/StrategyBuilder/TradePanel/TradeGraph/TradeGraphWrapper.js
@@ -6,7 +6,7 @@ const TradeGraphWrapper = styled.div`
   padding: 10px;
   margin: 15px 10px 15px 10px;
   border-radius: 10px;
-  width: 68%;
+  width: 69%;
   background-color: #1b1e22;
   box-shadow: 5px 5px 5px 0px rgba(0, 0, 0, 0.23);
   height: 350px;
