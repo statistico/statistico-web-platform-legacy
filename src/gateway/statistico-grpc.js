ea3b411a389844bea5dbd395cadcfea41c7f66bf
Joe Sweeny <joesweeny@intelligencefusion.co.uk>
2021-03-10 15:16:04 +0000

    #144 Move server host into env variable

--- src/gateway/statistico-grpc.js
+++ src/gateway/statistico-grpc.js
@@ -1,12 +1,10 @@
 import strategyTradeRequest from './grpc-request';
-import { StrategyServiceClient } from '../proto/strategy_grpc_web_pb';
+import StrategyClient from './grpc-client';
 
 const fetchStrategyTrades = (filters, updateFunc, endFunc, errorFunc) => {
   const request = strategyTradeRequest(filters);
 
-  const client = new StrategyServiceClient('http://localhost:8080', {}, {});
-
-  const stream = client.strategyTradeSearch(request, {});
+  const stream = StrategyClient().strategyTradeSearch(request, {});
 
   stream.on('data', (t) => {
     updateFunc(t.toObject());
