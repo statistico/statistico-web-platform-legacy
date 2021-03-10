0ebe43c3d3eed3f625d82f788fc37813d1bcd681
Joe Sweeny <joesweeny@intelligencefusion.co.uk>
2021-03-10 13:20:20 +0000

    Update responsive font size in season row wrapper

--- src/components/Trading/StrategyBuilder/TradePanel/CompetitionList/CompetitionRow/SeasonRow/SeasonRowWrapper.js
+++ src/components/Trading/StrategyBuilder/TradePanel/CompetitionList/CompetitionRow/SeasonRow/SeasonRowWrapper.js
@@ -2,8 +2,12 @@ import styled from 'styled-components';
 import { Tr } from 'react-super-responsive-table';
 
 const SeasonRowWrapper = styled(Tr)`
-  font-size: 12px;
+  font-size: 14px;
   color: #22ccde;
+
+  @media (max-width: 768px) {
+    font-size: 12px;
+  }
 `;
 
 export default SeasonRowWrapper;
