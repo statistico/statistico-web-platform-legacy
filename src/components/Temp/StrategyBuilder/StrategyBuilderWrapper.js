24b4790dacdb84d0359d192c6615ba582f62486e
Joe Sweeny <joesweeny@intelligencefusion.co.uk>
2021-03-11 20:48:06 +0000

    Remove old code

--- src/components/Temp/StrategyBuilder/StrategyBuilderWrapper.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const StrategyBuilderWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-`;
-
-export default StrategyBuilderWrapper;
