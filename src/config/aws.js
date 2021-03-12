43393cf6006a8b4ab83da5470031b3c4bdc842c8
Joe Sweeny <joesweeny@intelligencefusion.co.uk>
2021-03-12 16:37:56 +0000

    Add .env.example and gitignore env handling

---
+++ src/config/aws.js
@@ -0,0 +1,7 @@
+const aws = {
+  UserPoolId: process.env.REACT_APP_AWS_USER_POOL_ID,
+  ClientId: process.env.REACT_APP_AWS_USER_POOL_CLIENT_ID,
+  Region: process.env.REACT_APP_AWS_REGION,
+};
+
+export default aws;
