226e8c3ce7d38b1fa51cf2aba208adc25ead8e2b
Joe Sweeny <joesweeny84@hotmail.com>
2021-03-28 09:01:55 +0100

    Update styling of strategy builder header

--- src/components/Trading/StrategyBuilder/StrategyBuilderHeader/StrategyBuilderHeaderWrapper.js
+++ src/components/Trading/StrategyBuilder/StrategyBuilderHeader/StrategyBuilderHeaderWrapper.js
@@ -6,6 +6,9 @@ const StrategyBuilderHeaderWrapper = styled.div`
   justify-content: space-between;
   align-items: center;
   width: 100%;
+  padding: 20px;
+  border-radius: 10px;
+  background-color: #1b1e22;
 `;
 
 export default StrategyBuilderHeaderWrapper;
