4c27c2526433df762404f3636843e8b264a80a00
Joe Sweeny <joesweeny84@hotmail.com>
2021-05-07 11:32:21 +0100

    Add homepage and wrapper components

---
+++ src/components/HomePage/HomePageWrapper.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const HomePageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding: 20px;
+`;
+
+export default HomePageWrapper;
