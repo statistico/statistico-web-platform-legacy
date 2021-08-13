a6c5822252b1f21ddaaf543fe0ec60613db32c42
Joe Sweeny <joesweeny84@hotmail.com>
2021-08-13 17:19:06 +0100

    Remove padding from icon types

--- src/components/Icon/Icon.types.ts
+++ src/components/Icon/Icon.types.ts
@@ -1,9 +1,5 @@
 export type IconProps = {
   colour: string;
   height: number;
-  paddingBottom?: number | null;
-  paddingLeft?: number | null;
-  paddingRight?: number | null;
-  paddingTop?: number | null;
   width: number;
 }
