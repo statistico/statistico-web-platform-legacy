c230b1bc89ab54a4f99cedeb281dfb06d10981df
Joe Sweeny <joesweeny84@hotmail.com>
2021-08-13 21:51:07 +0100

    Update competition select styling

--- src/components/CompetitionSelect/CompetitionSelect.styles.ts
+++ src/components/CompetitionSelect/CompetitionSelect.styles.ts
@@ -5,11 +5,7 @@ const CompetitionSelectWrapper = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  padding: 10px;
-  background-color: #a1a9b1;
   border-radius: 10px;
-  box-shadow: 5px 5px 5px 0 rgba(0, 0, 0, 0.23);
-  color: #1b1e22;
 
   p {
     padding: 10px 0 15px 5px;
